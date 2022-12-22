491d8e1fb088099ed6451efc18c00de1526b1067
Abhishek2020xyz <116792007+Abhishek2020xyz@users.noreply.github.com>
2022-12-22 19:27:20 +0530

    Add files via upload

--- ExpenseForm.js
+++ ExpenseForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 import './ExpenseForm.css';
-const ExpenseForm = () =>{
+const ExpenseForm = (props) =>{
     const [enteredTitle, setEnteredTitle] = useState('')
     const [enteredAmount, setEnteredAmount] = useState('')
     const [enteredDate, setEnteredDate] = useState('')
@@ -24,22 +24,38 @@ const ExpenseForm = () =>{
             amount:enteredAmount,
             date:new Date(enteredDate)
         }
-        console.log(expenseData)
+    props.onSaveExpenseData(expenseData);
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('')
+
+
     }
 return (
 <form onSubmit={submitHandler}>
     <div className="new-expense__controls">
     <div className="new-expense__controls">
     <label>Title</label>
-    <input type='text' onChange={titleChangeHandler}/>
+    <input 
+    type='text' 
+    value={enteredTitle}
+    onChange={titleChangeHandler}/>
     </div>
     <div className="new-expense__controls">
     <label>Amount</label>
-    <input type='number' onChange={amountChangeHandler}/>
+    <input 
+    type='number' 
+    value={enteredAmount}
+    onChange={amountChangeHandler}/>
     </div>
     <div className="new-expense__controls">
     <label>Date</label>
-    <input type='date' min="01-10-2019" max="31-12-2022" onChange={dateChangeHandler}/>
+    <input 
+    type='date' 
+    min="01-10-2019" 
+    max="31-12-2022" 
+    value={enteredDate}
+    onChange={dateChangeHandler}/>
     </div>
         
     </div>
